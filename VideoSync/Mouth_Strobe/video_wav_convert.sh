275dcca8a09ddf4bba0c02f82c48df0197035866
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2013-07-29 15:31:38 -0400

    fixed so script encodes at 44.1 kHz

---
+++ VideoSync/Mouth_Strobe/video_wav_convert.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Uses ffmpeg to convert video files in a list into .wav files.
+# To run, simply run script with the file extension of the video files as the first
+# argument.
+#
+# ex: ./video_wav_convert.sh m4v 
+#
+# To convert all the .m4v video files in the directory to .wav audio files.
+
+filetype=$1
+
+echo -e '\n \n Stripping audio for following files:\n --------------------------------------'
+
+# Prints out the names of all the files to convert.
+for f in *.$filetype;
+do
+echo $f
+done
+
+echo -e '\n \n' # Space
+
+# Strips the audio of each file and makes a wav file using ffmpeg.
+for f in *.$filetype;
+do
+ffmpeg -i "$f" -f wav -vn -ar 44100 "${f%%.$filetype}_VidAudio.wav" # the convert command
+done
+
+echo -e '\n Conversion Complete. \n'  # End of script

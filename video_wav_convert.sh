2ba0cbf58fa59346031815f31254f6a07386bbc8
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2013-07-31 15:18:55 -0400

    stripts out one channel after converting

--- video_wav_convert.sh
+++ video_wav_convert.sh
@@ -10,7 +10,8 @@
 # To convert all the .m4v video files in the directory to .wav audio files.
 
 filetype=$1
-sampleRate=44100  # Change number to change sampling rate
+sampleRate=44100 # Change number to change sampling rate
+channel=2        # select which stereo channel to pull (1 for right, 2 for left)
 
 echo -e '\n \n Stripping audio for following files:\n --------------------------------------'
 
@@ -23,18 +24,22 @@ done
 echo -e '\n' # Space
 
 read -r -p "Would you like to continue? [Y/n] " response
+# If user selects 'y'
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
 	# for each file in directory of specified file type..
 	for f in *.$filetype;
 	do
 	# Converts files using ffmpeg
-	ffmpeg -i "$f" -f wav -vn -ar $sampleRate  "${f%%.$filetype}_VidAudio.wav" 
+	ffmpeg -i "$f" -f wav -vn -ar $sampleRate  "${f%%.$filetype}_VidAudio_bothChan.wav"
+	# Strips out one channel for stereo and saves as wav.
+	sox "${f%%.$filetype}_VidAudio_bothChan.wav" "${f%%.$filetype}_VidAudio.wav" remix $channel
+	rm "${f%%.$filetype}_VidAudio_bothChan.wav" # remove stereo file
 	done
 echo -e '\n Conversion Complete. \n'  # End of script
 
+# If user selects 'n'
 else
-
 echo -e '\n Exiting Script.'
 
 fi

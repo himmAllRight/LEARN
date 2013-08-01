b520bc7a8aa598b1907f6213e681dc63ce3b5559
Ryan Himmelwright <ryan@ryanhimmelwright.com>
2013-08-01 14:54:53 -0400

    Bunch new options added

--- video_wav_convert.sh
+++ video_wav_convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 7/30/2013
+# 8/01/2013
 #
 # Uses ffmpeg to convert video files in a list into .wav files.
 # To run, simply run script with the file extension of the video files as the first
@@ -13,15 +13,15 @@ filetype=$1
 sampleRate=44100 # Change number to change sampling rate
 channel=2        # select which stereo channel to pull (1 for right, 2 for left)
 
-echo -e '\n \n Stripping audio for following files:\n --------------------------------------'
+echo -e '\n Stripping audio for following files:\n --------------------------------------'
 
 # Prints out the names of all the files to convert.
 for f in *.$filetype;
 do
-echo $f
+	echo $f
 done
 
-echo -e '\n' # Space
+echo -e '' # Space
 
 read -r -p "Would you like to continue? [Y/n] " response
 # If user selects 'y'
@@ -36,10 +36,54 @@ then
 	sox "${f%%.$filetype}_VidAudio_bothChan.wav" "${f%%.$filetype}_VidAudio.wav" remix $channel
 	rm "${f%%.$filetype}_VidAudio_bothChan.wav" # remove stereo file
 	done
-echo -e '\n Conversion Complete. \n'  # End of script
+echo -e 'Conversion Complete. \n'  # End of script
 
-# If user selects 'n'
+# Ask to make package of wav files.
+read -r -p "Would you like to make a package of audio files? [Y/n]: " response
+# Yes
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	read -r -p "Do you also want to include the $filetype files? [Y/n]: "
+	if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+	then
+		echo -e "Starting to create package with .wav and $filetype files... \n"
+		mkdir videoAudioPackage			# Make package folder
+		echo 'adding wave files...'
+		mv *_VidAudio.wav videoAudioPackage	# Move wav files to package folder
+		echo "adding $filetype files..."
+		cp *.$filetype videoAudioPackage	# Copy video files to package folder
+		echo 'creating package...'
+		tar -czvf "videoAudioPackage"_`date +"%Y%m%d%H%M"`.tar.gz videoAudioPackage
+		echo -e 'Package created. \n'
+	else
+		echo -e 'Starting to create package with just .wav files...\n' # put files in package
+		mkdir videoAudioPackage
+		mv *_VidAudio.wav videoAudioPackage
+		tar -czvf "videoAudioPackage"_`date +"%Y%m%d%H%M"`.tar.gz videoAudioPackage
+		echo -e 'Package created. \n'
+	fi
+# If no (for package make)
 else
-echo -e '\n Exiting Script.'
+	echo -e 'Exiting Script.'
+fi
 
+# Asks user if they want to delete wav files.
+read -r -p "Do you want to delete the wav files stripped from the video? (files still exist in package) [Y/n]: " response
+# Yes, delete converted wav files.
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	echo -e 'Removing files'
+	rm -r videoAudioPackage/*	# remove wav files
+	rm -r videoAudioPackage		# remove directory
+	echo 'Exiting Script.'
+# No, move wav files into main Directory and remove package folder.
+else
+	echo 'Leaving wav files in directory'
+	mv videoAudioPackage/* .
+	rm -r videoAudioPackage
+	echo 'Exiting Script.'
+fi
+# If user selects 'n' (for convert)
+else
+	echo -e 'Exiting Script. \n'
 fi
